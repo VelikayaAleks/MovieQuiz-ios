8f700fd7f350da3e0611ca1456720dec71396425
Aleksandra Velikaia <sashuunka@yandex.ru>
2024-03-26 14:59:48 +0300

    Рефакторинг MovieQuizViewController в MovieQiuzPresenter

---
+++ MovieQuiz/Presentation/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//  Created by Александра Великая on 26.03.2024.
+//
+
+import Foundation
---
+++ MovieQuizTests/MovieQuizPresenterTests.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizPresenterTests.swift
+//  MovieQuizTests
+//
+//  Created by Александра Великая on 26.03.2024.
+//
+
+import Foundation
